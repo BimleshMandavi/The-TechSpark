76971748244078a0f76b53b9024b3500328a0976
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-11-27 15:50:45 +0530

    Edit meta tags for SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           property="og:image"
           content="/Tech.png"
         />
-        <meta property="og:url" content="https://www.yourdomain.com" />
+        <meta property="og:url" content="https://www.gotechspark.com" />
 
         {/* Structured Data */}
         <script
@@ -44,7 +44,7 @@ export default function Home() {
               "@type": "Organization",
               "name": "Tech Spark",
               "url": "https://www.gotechspark.com",
-              "logo": "https://www.yourdomain.com/Tech.png",
+              "logo": "https://www.gotechspark.com/Tech.png",
               "contactPoint": {
                 "@type": "ContactPoint",
                 "phone": "+91-6265761531",
@@ -53,9 +53,9 @@ export default function Home() {
                 "availableLanguage": ["Hindi,English"],
               },
               "sameAs": [
-                "https://www.facebook.com/yourcompany",
-                "https://twitter.com/yourcompany",
-                "https://www.linkedin.com/company/yourcompany",
+                "https://www.facebook.com/TechSpark",
+                "https://twitter.com/TechSpark",
+                "https://www.linkedin.com/company/TechSpark",
               ],
               "description":
                 "Tech Spark is a top web and mobile app software development company offering services like web and app development, logo design, UI/UX design, dashboard design, and server management.",
